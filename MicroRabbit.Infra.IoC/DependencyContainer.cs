a7d494bde196cff0f4b035e93f371da3f570db1b
nadaibrahim <nada.ibrahim.de@gmail.com>
2025-04-13 21:23:18 +0200

    Initial Commit of infrastructure dependency project

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Register the Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+        }
+
+    }
+}
---
+++ MicroRabbit.Infra.IoC/_GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using MicroRabbit.Domain.Core.Bus;
+global using MicroRabbit.Infra.Bus;
+global using Microsoft.Extensions.DependencyInjection;
